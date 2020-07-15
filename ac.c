20b62ab0e713497c8e7e77bf7d68d370f42ab780
root <root@raspberrypi>
2020-07-15 07:12:32 +0100

    Improved timings

--- ac.c
+++ ac.c
@@ -157,26 +157,16 @@ int main(int argc, char *argv[])
   uint32_t outPin = 17;            // The Broadcom pin number the signal will be sent on
   int frequency = 38000;           // The frequency of the IR signal in Hz
   double dutyCycle = 0.5;          // The duty cycle of the IR signal. 0.5 means for every cycle,
-  // the LED will turn on for half the cycle time, and off the other half
-  /*	int leadingPulseDuration = 9000; // The duration of the beginning pulse in microseconds
-	int leadingGapDuration = 4500;   // The duration of the gap in microseconds after the leading pulse
-	int onePulse = 562;              // The duration of a pulse in microseconds when sending a logical 1
-	int zeroPulse = 562;             // The duration of a pulse in microseconds when sending a logical 0
-	int oneGap = 1688;               // The duration of the gap in microseconds when sending a logical 1
-	int zeroGap = 562;               // The duration of the gap in microseconds when sending a logical 0
-	int sendTrailingPulse = 1;       // 1 = Send a trailing pulse with duration equal to "onePulse"
-	                                 // 0 = Don't send a trailing pulse
-					 */
 	
   int
-    headerMarkDuration=3500,
-    headerSpaceDuration=1800,
-    widespaceMarkDuration=0,
-    widespaceSpaceDuration=10000,
-    zeroMarkDuration=420,
-    zeroSpaceDuration=470,
-    oneMarkDuration=420,
-    oneSpaceDuration=1350;
+    headerMarkDuration=3515,
+    headerSpaceDuration=1750,
+    widespaceMarkDuration=435,
+    widespaceSpaceDuration=10330,
+    zeroMarkDuration=435,
+    zeroSpaceDuration=435,
+    oneMarkDuration=435,
+    oneSpaceDuration=1310;
 
   symbolDefinition configSymbols[] = 
     {{'H',headerMarkDuration,headerSpaceDuration},
